4d0052ed0cb218f9087f4bfa8671f9d144e0c8c1
Chase Williams <bornofawesomeness@gmail.com>
2017-11-09 11:46:52 -0500

    Update app.js

--- tutorial/js/app.js
+++ tutorial/js/app.js
@@ -1,20 +1,10 @@
 
-
 var app = new Vue({
-  el: '#app',
-  data: {
-    link: "https://imgs.xkcd.com/comics/thermostat.png"
-  },
-  methods: {
-      get_data: function () {
-          var num = Math.floor(Math.random() * 700)
-          var url = "https://xkcd.com/" + num + "/info.0.json"
-          $.getJSON(url, 
-          function (data) {
-            console.log(data)
-              //Vue.set(app, "link", data.img)
-          })
-          
-      }
-  }
-})
+    el: '#app',
+    data: {
+      
+    },
+    methods: {
+        
+    }
+  })
